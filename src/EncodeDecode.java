f3b6fd7a543c72924acd78f4ee26c7ddbb5ac3dc
Louis Justus Siegrist <louis.siegrist@gmx.net>
2016-11-18 10:04:54 +0100

    added doc

--- src/EncodeDecode.java
+++ src/EncodeDecode.java
@@ -1,10 +1,21 @@
 /**
- * Created by louisjustussiegrist on 18.11.16.
+ * This interface provides basic encode and decode methods for EnOcean telegrams. Including description.
+ *
+ * @author louisjustussiegrist
  */
 public interface EncodeDecode {
 
-
+    /**
+     * encode provides an example telegram hex for this packet type.
+     *
+     * @return telegram hex
+     */
     String encode ();
 
+    /**
+     * decode splits telegram in parts
+     *
+     * @param hexTelegram
+     */
     void decode (String hexTelegram);
 }
