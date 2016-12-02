9e905e692e3f5485762014ec36b405243b396097
Gr33nOpo55um <p.stegmueller@gmail.com>
2016-12-02 12:00:27 +0100

    fixes, .ignore and refactoring

--- src/ch/bfh/gr33nopo55um/enocean/telegram/RadioSubTel.java
+++ src/ch/bfh/gr33nopo55um/enocean/telegram/RadioSubTel.java
@@ -14,6 +14,19 @@ import javax.persistence.Id;
 public class RadioSubTel extends TelegramHeader {
 
 
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int subTelNum;
+    private String destinationID;
+    private int dbm;
+    private int securityLevel;
+    private String timeStamp;
+    private String tickSubTel;
+    private String dbmSubTel;
+    private String statusSubTel;
+
     @Override
     public String toString() {
         return "RadioSubTel{" +
@@ -92,20 +105,6 @@ public class RadioSubTel extends TelegramHeader {
         this.statusSubTel = statusSubTel;
     }
 
-    @Id
-
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private int subTelNum;
-    private String destinationID;
-    private int dbm;
-    private int securityLevel;
-    private String timeStamp;
-    private String tickSubTel;
-    private String dbmSubTel;
-    private String statusSubTel;
-
     /**
      * encodeTelegram provides an example telegram hex for this packet type.
      *
@@ -115,7 +114,6 @@ public class RadioSubTel extends TelegramHeader {
     public String encodeTelegram() {
         return null;
     }
-
 
 
     /**
