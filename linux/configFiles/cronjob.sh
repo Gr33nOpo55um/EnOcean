3ed78fe6c4486e840174ec1d9d4c30e128ee29eb
Silas Stegmüller <Gr33nOpo55um@users.noreply.github.com>
2017-01-28 09:11:57 +0100

    Update cronjob
    added -a option to rsync.

--- linux/configFiles/cronjob.sh
+++ linux/configFiles/cronjob.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-  systemctl enable cronie
+sudo systemctl enable cronie
 
 sudo systemctl start cronie
 
@@ -10,6 +10,6 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub logger@192.168.1.45
 ssh logger@192.168.1.45
 
 
-rsync /opt/enoca/logs/telegrams.log logger@192.168.1.45:/var/log/enoca
+rsync -a /opt/enoca/logs/telegrams.log logger@192.168.1.45:/var/log/enoca
 
-(crontab -l 2>/dev/null; echo "* * * * * rsync /opt/enoca/logs/telegrams.log logger@192.168.1.45:/var/log/enoca/$HOSTNAME/ >/dev/null 2>&1")| crontab -
+(crontab -l 2>/dev/null; echo "* * * * * rsync -a /opt/enoca/logs/telegrams.log logger@192.168.1.45:/var/log/enoca/$HOSTNAME/ >/dev/null 2>&1")| crontab -
